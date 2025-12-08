6293f51b3cd9b528e0c6e5541d051fec829d5a7b
lyh543 <lyh543@outlook.com>
2025-12-08 14:18:20 +0800

    chore: add sentry

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://6f9b7d7e4104464f826138049a9d6722@o4505519988998144.ingest.us.sentry.io/4505519990439936",
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
+});
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
